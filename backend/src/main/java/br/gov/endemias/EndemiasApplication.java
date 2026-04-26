936dd1acecfcf1a33c453d98c66fcdf09048897c
Alison-Andrade <sidneialison@gmail.com>
2026-04-26 15:49:13 -0300

    feat: initial commit beckend

---
+++ backend/src/main/java/br/gov/endemias/EndemiasApplication.java
@@ -0,0 +1,13 @@
+package br.gov.endemias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndemiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndemiasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/gov/endemias/EndemiasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.endemias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndemiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
